3b4a6605f8571cfbad40e6d37b62051e72b63b25
AdamWD <falco1016@gmail.com>
2018-03-23 18:09:06 +0800

    back up after Migrating to Xcode 8 and Swift 3.0(Ep20)

--- youtube/Helps/Extensions.swift
+++ youtube/Helps/Extensions.swift
@@ -27,7 +27,7 @@ extension UIView {
     }
 }
 
-let imageCache = NSCache<AnyObject, AnyObject>()
+let imageCache = NSCache<NSString, UIImage>()
 
 class CustomImageView: UIImageView {
     
@@ -38,7 +38,7 @@ class CustomImageView: UIImageView {
         
         image = nil
         
-        if let imageFromCache = imageCache.object(forKey: urlString as AnyObject) as? UIImage
+        if let imageFromCache = imageCache.object(forKey: urlString as NSString)
         {
             self.image = imageFromCache
             return
@@ -58,7 +58,7 @@ class CustomImageView: UIImageView {
                     self.image = imageToCache
                 }
                 
-                imageCache.setObject(imageToCache!, forKey: urlString as AnyObject)
+                imageCache.setObject(imageToCache!, forKey: urlString as NSString)
             })
             
         }).resume()
